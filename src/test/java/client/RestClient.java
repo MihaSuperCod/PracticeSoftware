821ec65650a1eda1df546e0368c438b375648257
MihaSuperCod <mihaelamoga23@gmail.com>
2026-02-26 20:37:39 +0200

    Refactor BE tests

--- src/test/java/client/RestClient.java
+++ src/test/java/client/RestClient.java
@@ -10,27 +10,22 @@ public class RestClient {
     //Pe baza configurarilor trebuie sa pot sa execut orice actiune (get, post, put, delete)
 
 
-    public Response performRequest(String requestType, RequestSpecification request, String endpoint){
-        switch (requestType){
+    public Response performRequest(String requestType, RequestSpecification request, String endpoint) {
+        switch (requestType) {
             case "POST":
-                prepareClient(request).post(endpoint);
-                break;
+                return prepareClient(request).post(endpoint);
             case "PUT":
-                prepareClient(request).put(endpoint);
-                break;
+                return prepareClient(request).put(endpoint);
             case "GET":
-                prepareClient(request).get(endpoint);
-                break;
+                return prepareClient(request).get(endpoint);
             case "DELETE":
-                prepareClient(request).delete(endpoint);
-                break;
+                return prepareClient(request).delete(endpoint);
         }
         return null;
     }
 
     public RequestSpecification prepareClient(RequestSpecification request){
-        RestAssured.baseURI = "https://api.practicesoftwaretesting.com";
-        request = RestAssured.given();
+        request.baseUri("https://api.practicesoftwaretesting.com");
         request.header("Content-type", "application/json");
         request.header("Accept", "application/json");
         return request;
